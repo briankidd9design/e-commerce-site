d4bf0da8dbb0251b3cffddfcb22486002bf5ae62
Brian Kidd <brian.kidd.one@gmail.com>
2024-12-18 14:45:47 -0800

    adds cart image, name, quantity, price and delete button

---
+++ src/components/CartProduct.js
@@ -0,0 +1,57 @@
+import {
+  MinusIcon,
+  PlusIcon,
+  XMarkIcon,
+  XCircleIcon,
+} from "@heroicons/react/24/outline";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function CartProduct({ product }) {
+  console.log(product.image);
+  return (
+    <div className="flex justify-between space-x-4 hover:shadow-lg hover:border-opacity-50 border border-opacity-0 rounded-md p-4 bg-white">
+      <Link
+        href={`/products/${product.id}`}
+        className="flex items-center space-x-4 group"
+      >
+        <div className="relative w-20 h-20 group-hover:scale-110 transition-transform">
+          <Image
+            src={product.image}
+            alt={product.name}
+            fill
+            style={{ objectFit: "contain" }}
+          />
+        </div>
+        <p className="font-semibold text-xl group-hover:underline">
+          {product.name}
+        </p>
+      </Link>
+      <div className="flex items-center">
+        <div className="flex items-center space-x-3">
+          <button
+            disabled={product.quantity <= 1}
+            // onClick={() => setCount((count) => count - 1)}
+            className="p-1 rounded-md hover:bg-rose-100 hover:text-rose-500"
+          >
+            <MinusIcon className="w-6 h-6 flex-shrink-0" />
+          </button>
+          <p className="font-semibold text-xl">{product.quantity}</p>
+          <button
+            // onClick={() => setCount((count) => count + 1)}
+            className="p-1 rounded-md hover:bg-green-100 hover:text-green-500"
+          >
+            <PlusIcon className="w-6 h-6 flex-shrink-0" />
+          </button>
+        </div>
+        <p className="fon-semibold text-xl ml-16">
+          <XMarkIcon className="didden w-4 h-4 text-gray-500 sm:inline-block mr-4 mb-1" />
+          {product.formattedPrice}
+        </p>
+        <button className="ml-4 hover:text-red-500">
+          <XCircleIcon className="w-6 h-6 flex-shrink-0 opacity-50 hover:opacity-100 transition-opacity" />
+        </button>
+      </div>
+    </div>
+  );
+}
